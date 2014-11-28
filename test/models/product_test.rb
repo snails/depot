a22bd10a5a012d49244e05cc7a8d6a46c2b8b9bf
DavidWei <godhuyang@gmail.com>
2014-11-28 18:15:00 +0800

    Add validations and test to Products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  fixtures :products
+
+  test "Product属性不能为空" do 
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "价格必须大于0" do
+    product = Product.new(title: "My Book Title",
+                          description: "hello world", image_url: "fantacy.jpg")
+    product.price = -3
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "测试101",
+               description: "描述12313",
+               price: 10.12,
+               image_url: image_url)
+  end
+
+  test "图片后缀必须是jpg, gif或png" do
+    good = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    good.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} should be invlid"
+    end
+  end
+
+  test "product is not valid without a unique title" do 
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy", price: 1, image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title] 
+  end
+
+end 
